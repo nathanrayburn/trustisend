94460ff79c4a74d8e75f6f16aa686405a6d1df7e
nathan <nathanarmand@hotmail.com>
2024-08-22 14:19:07 +0200

    init gc storage project

---
+++ dev/test-gc-storage/gc-storage/src/main/java/demo_storage/gc_storage/GcStorageApplication.java
@@ -0,0 +1,13 @@
+package demo_storage.gc_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcStorageApplication.class, args);
+	}
+
+}
---
+++ dev/test-gc-storage/gc-storage/src/test/java/demo_storage/gc_storage/GcStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package demo_storage.gc_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
