e88d39beb300d6c2371ba1cc61ace0dfb68e4adc
nathan <nathanarmand@hotmail.com>
2024-08-22 13:52:21 +0200

    updated index

--- app/src/main/java/dev/test/social_login/controller/HomeController.java
+++ app/src/main/java/dev/test/social_login/controller/HomeController.java
@@ -3,12 +3,14 @@ package dev.test.social_login.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
-@RestController
+@Controller
 public class  HomeController {
     @GetMapping("/")
-    public String home(){
-        return "This will be the landing page.";
+    public String home(Model model){
+        model.addAttribute("test","Test value message");
+        return "index";
     }
     @GetMapping("/secured")
     public String secured(){
