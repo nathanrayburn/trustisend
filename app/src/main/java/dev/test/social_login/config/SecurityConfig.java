0861ee8c958e600e1980365a60fbd07c116a96f8
BoluxByte <felix.breval.spotify@gmail.com>
2024-08-23 11:30:25 +0200

    modified login page

--- app/src/main/java/dev/test/social_login/config/SecurityConfig.java
+++ app/src/main/java/dev/test/social_login/config/SecurityConfig.java
@@ -13,14 +13,50 @@ import static org.springframework.security.config.Customizer.*;
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http
-                .authorizeHttpRequests( auth -> {
-                    auth.requestMatchers("/").permitAll();
-                    auth.anyRequest().authenticated();
-                })
-                .formLogin(withDefaults())
-                .oauth2Login(withDefaults())
-                .build();
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user1 = User.withUsername("user1")
+            .password(passwordEncoder().encode("user1Pass"))
+            .roles("USER")
+            .build();
+        UserDetails user2 = User.withUsername("user2")
+            .password(passwordEncoder().encode("user2Pass"))
+            .roles("USER")
+            .build();
+        UserDetails admin = User.withUsername("admin")
+            .password(passwordEncoder().encode("adminPass"))
+            .roles("ADMIN")
+            .build();
+        return new InMemoryUserDetailsManager(user1, user2, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+            .disable()
+            .authorizeRequests()
+            .antMatchers("/admin/**")
+            .hasRole("ADMIN")
+            .antMatchers("/anonymous*")
+            .anonymous()
+            .antMatchers("/login*")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .formLogin()
+      .loginPage("/login.html")
+      .loginProcessingUrl("/perform_login")
+      .defaultSuccessUrl("/homepage.html", true)
+      .failureUrl("/login.html?error=true")
+      .failureHandler(authenticationFailureHandler())
+      .and()
+      .logout()
+      .logoutUrl("/perform_logout")
+      .deleteCookies("JSESSIONID")
+      .logoutSuccessHandler(logoutSuccessHandler());
+      return http.build();
+    }
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
