89db4665aebdc9d19f367bb9647c4c4a9dd0e91f
nathan <nathanarmand@hotmail.com>
2024-08-26 15:28:27 +0200

    test get secret from mount

--- app/src/main/java/dev/test/social_login/SocialLoginApplication.java
+++ app/src/main/java/dev/test/social_login/SocialLoginApplication.java
@@ -2,6 +2,14 @@ package dev.test.social_login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class SocialLoginApplication {
@@ -10,4 +18,17 @@ public class SocialLoginApplication {
 		SpringApplication.run(SocialLoginApplication.class, args);
 	}
 
+}
+
+@Component
+class SecretReader implements CommandLineRunner {
+
+	@Value("${secret.filepath}")
+	private String secretFilePath;
+
+	@Override
+	public void run(String... args) throws Exception {
+		String secret = new String(Files.readAllBytes(Paths.get(secretFilePath)));
+		System.out.println("Secret: " + secretFilePath);
+	}
 }
