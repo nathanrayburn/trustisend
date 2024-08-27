bb4b9d37c9a278f9d928f9e6d46a1f357c7add3e
nathan <nathanarmand@hotmail.com>
2024-08-27 13:12:45 +0200

    view dev project

---
+++ dev/view-dev/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+
+@Controller
+public class  HomeController {
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("test","Test value message");
+        return "index";
+    }
+    @GetMapping("/secured")
+    public String secured(){
+        return "Hello, Secured!";
+    }
+}
