e6377720f7537cc441e5c58b47cc5c4560ca3c1d
nathan <nathanarmand@hotmail.com>
2024-08-30 12:45:09 +0200

    tracking and logging files to firestore

--- app/src/main/java/dev/test/trustisend/service/FileService.java
+++ app/src/main/java/dev/test/trustisend/service/FileService.java
@@ -2,11 +2,16 @@ package dev.test.trustisend.service;
 
 
 import dev.test.trustisend.dto.FileDto;
+import dev.test.trustisend.entity.ActiveFile;
+import dev.test.trustisend.entity.Group;
 import dev.test.trustisend.entity.InputFile;
+import dev.test.trustisend.entity.User;
 import dev.test.trustisend.exception.BadRequestException;
 import dev.test.trustisend.exception.GCPFileUploadException;
 import dev.test.trustisend.util.DataBucketUtil;
+import dev.test.trustisend.util.FirestoreUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,6 +20,8 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -26,12 +33,31 @@ public class FileService{
 
     private final DataBucketUtil dataBucketUtil;
 
+    private final FirestoreUtil firestoreUtil;
+
     public List<InputFile> uploadFiles(MultipartFile[] files) {
 
         List<InputFile> inputFiles = new ArrayList<>();
 
-        String uID = java.util.UUID.randomUUID().toString();
+        //String uID = java.util.UUID.randomUUID().toString();
+        // Create a new group
+        // todo some how get the user
+        // using fictive user
+        User user = new User("test@heig-vd.ch", "testpassword");
 
+        Group group = new Group(user.getEmail(), LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), 0);
+
+        try{
+            group = firestoreUtil.createGroup(group);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new BadRequestException("Error occurred while uploading: " + e.getMessage());
+        }
+
+
+        Group finalGroup = group;
+        System.out.println("Group created successfully with ID: " + finalGroup.getGroupUUID());
         Arrays.asList(files).forEach(file -> {
             String originalFileName = file.getOriginalFilename();
             if(originalFileName == null){
@@ -41,11 +67,17 @@ public class FileService{
             Path path = new File(originalFileName).toPath();
 
             try {
+
                 String contentType = Files.probeContentType(path);
-                FileDto fileDto = dataBucketUtil.uploadFile(file, originalFileName, contentType, uID);
+                FileDto fileDto = dataBucketUtil.uploadFile(file, originalFileName, contentType, finalGroup.getGroupUUID());
+                // Add file to Firestore
+                ActiveFile activeFile = new ActiveFile(finalGroup, originalFileName);
+                activeFile = firestoreUtil.createActiveFile(activeFile);
+
+                System.out.println("File created successfully with ID: " + activeFile.getFileUUID());
 
                 if (fileDto != null) {
-                    inputFiles.add(new InputFile(uID, fileDto.getFileName(), fileDto.getFileUrl()));
+                    inputFiles.add(new InputFile(finalGroup.getGroupUUID(), fileDto.getFileName(), fileDto.getFileUrl()));
                 }
             } catch (Exception e) {
                 throw new GCPFileUploadException("Error occurred while uploading: " + e.getMessage());
