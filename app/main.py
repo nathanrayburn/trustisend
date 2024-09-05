277c82b8f9c313dc6a47f4d2764d681603da9bef
Felix Breval <felix.breval@heig-vd.ch>
2024-09-06 00:23:59 +0200

    changes to main for requests

--- app/main.py
+++ app/main.py
@@ -13,14 +13,8 @@ import schedule
 from waitress import serve
 import threading  # Import threading
 import time  # Import time for the scheduler loop
-import certifi
-import urllib3
-from urllib3 import encode_multipart_formdata
 
 app = Flask(__name__)
-
-# Create a PoolManager with certifi's CA certificates
-http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 
 tempDirectory = "temp/"
 url = "https://www.virustotal.com/api/v3/files"
@@ -45,11 +39,10 @@ bucketName = data['storage']['bucket']
 
 def getActiveFilesFromFirestore():
     activeFiles_ref = db.collection("files").where("scanStatus", "in", [FileScanStatus.PENDING.value])
-    # add the files with error status to the list
     errorFiles_ref = db.collection("files").where("scanStatus", "==", FileScanStatus.ERROR.value)
     active = activeFiles_ref.stream()
     error = errorFiles_ref.stream()
-    
+
     activeFiles = []
     for doc in active:
         activeFiles.append(
@@ -76,10 +69,8 @@ def selectFirstOldestFiles(numberOfFiles, activeFiles):
     
     files_with_timestamps = [(file, getTimestamp(file.group_uuid)) for file in activeFiles]
 
-    # Filter out files with a timestamp of 0
     valid_files = [file for file, timestamp in files_with_timestamps if timestamp != 0]
 
-    # Sort the valid files by their timestamp
     valid_files.sort(key=lambda x: getTimestamp(x.group_uuid))
     return valid_files[:numberOfFiles]
 
@@ -117,7 +108,6 @@ def readAPIKey():
 
 
 def sendToAPI(file):
-    urllib3.disable_warnings()
     fileName = tempDirectory + file.file_uuid + "." + getExtension(file.path)
     file_size = os.path.getsize(fileName)
     key = readAPIKey()
@@ -128,34 +118,22 @@ def sendToAPI(file):
 
     # If the file is larger than 32MB, use the upload URL method
     if file_size > 32 * 1024 * 1024:  # 32MB in bytes
-        response = http.request("GET", upload_url_endpoint, headers=headers)
-        if response.status == 200:
-            upload_url = json.loads(response.data.decode("utf-8"))["data"]
+        response = requests.get(upload_url_endpoint, headers=headers)
+        if response.status_code == 200:
+            upload_url = response.json()["data"]
             with open(fileName, "rb") as f:
-                fields = {
-                    'file': (fileName, f.read(), "application/octet-stream")
-                }
-                body, content_type = encode_multipart_formdata(fields)
-                upload_headers = headers.copy()
-                upload_headers["Content-Type"] = content_type
-                upload_response = http.request(
-                    "POST", upload_url, body=body, headers=upload_headers)
+                files = {"file": (fileName, f, "application/octet-stream")}
+                upload_response = requests.post(upload_url, files=files, headers=headers)
                 return upload_response
         else:
-            print(f"Failed to get upload URL: {response.data.decode('utf-8')}")
+            print(f"Failed to get upload URL: {response.text}")
             db.collection("files").document(file.file_uuid).update({"scanStatus": FileScanStatus.ERROR.value})
             return response
     else:
         # For files <= 32MB, use the standard upload method
         with open(fileName, "rb") as f:
-            fields = {
-                'file': (fileName, f.read(), "application/octet-stream")
-            }
-            body, content_type = encode_multipart_formdata(fields)
-            upload_headers = headers.copy()
-            upload_headers["Content-Type"] = content_type
-            response = http.request(
-                "POST", url, body=body, headers=upload_headers)
+            files = {"file": (fileName, f, "application/octet-stream")}
+            response = requests.post(url, files=files, headers=headers)
             return response
 
 
@@ -164,21 +142,10 @@ hashmap = {}
 
 
 def associateFileToAnalysisID(file, data):
-    # add to hashmap
     hashmap[file.file_uuid] = data
 
 
 def isExpectedResults(key, value):
-    '''
-                "malicious": x,
-                "suspicious": x,
-                "undetected": x,
-                "harmless": x,
-                "timeout": x,
-                "confirmed-timeout": x,
-                "failure": x,
-                "type-unsupported": x
-    '''
     if key == "malicious" and value > 0:
         return FileScanStatus.INFECTED
     elif key == "suspicious" and value > 0:
@@ -200,7 +167,7 @@ def followUpFileAnalysis():
     for file_uuid, data in hashmap.items():
         if handleFile(file_uuid, data):
             keys_to_remove.append(file_uuid)  # Mark for removal
-            continue  # Move to the next item if handleFile returns True
+            continue
 
     # Remove the keys after the iteration is complete
     for key in keys_to_remove:
@@ -217,34 +184,32 @@ def handleFile(file_uuid, data):
         "accept": "application/json",
         "x-apikey": key
     }
-    
-    response = http.request("GET", url, headers=headers)
-    if response.status == 200:
-        res = json.loads(response.data.decode("utf-8"))
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        res = response.json()
         if res['data']['attributes']['status'] == "completed":
             status = FileScanStatus.PENDING
-
             for key, value in res['data']['attributes']['stats'].items():
                 status = isExpectedResults(key, value)
                 if status == FileScanStatus.INFECTED or status == FileScanStatus.ERROR:
                     db.collection("files").document(file_uuid).update({"scanStatus": status.value})
-                    return True  # Early return to continue outer loop
+                    return True
 
             # update firestore
             db.collection("files").document(file_uuid).update({"scanStatus": status.value})
             if status == FileScanStatus.CLEAN:
                 print("File " + file_uuid + " is clean")
-                return True  # Mark for removal since it is clean
+                return True
         elif res['data']['attributes']['status'] == "queued":
             print("File " + file_uuid + " is queued")
         elif res['data']['attributes']['status'] == "in-progress":
             print("File " + file_uuid + " is in progress")
     else:
-        print("Error in follow up file analysis")
-        print(response.data.decode("utf-8"))
+        print("Error in follow-up file analysis")
+        print(response.text)
         db.collection("files").document(file_uuid).update({"scanStatus": FileScanStatus.ERROR.value})
-        return True  # Mark for removal since an error occurred
-    return False  # Do not mark for removal
+        return True
+    return False
 
 
 def scanNewFiles():
@@ -253,18 +218,16 @@ def scanNewFiles():
         activeFiles = getActiveFilesFromFirestore()
         if not activeFiles:
             logging.debug("No files found with PENDING status.")
-            return  # Return early if no files to process
+            return
 
         # Remove files that are in the hashmap
         activeFiles = [file for file in activeFiles if file.file_uuid not in hashmap]
         if not activeFiles:
             logging.debug("No new files found after filtering by hashmap.")
-            return  # Return early if no files to process
+            return
 
         # Select the first n oldest files
         filesToScan = selectFirstOldestFiles(data['antivirus']['file-scan-limit'], activeFiles)
-        for file in filesToScan:
-            logging.debug(f"Processing file: {file.group_uuid}, {file.file_uuid}, {file.path}, {file.scan_status}")
 
         # Get the files from cloud storage
         downloadFilesFromCloudStorage(filesToScan)
@@ -273,7 +236,7 @@ def scanNewFiles():
         for file in filesToScan:
             try:
                 res = sendToAPI(file)
-                if res.status == 200:
+                if res.status_code == 200:
                     associateFileToAnalysisID(file, res.json()['data'])
                     logging.debug(f"File {file.file_uuid} sent to API successfully.")
                 else:
@@ -296,41 +259,6 @@ def scanNewFiles():
         logging.debug(f"An error occurred while scanning files: {e}")
         import traceback
         traceback.print_exc()
-    try:
-        # Get all files from Firestore that have scanStatus = PENDING
-        activeFiles = getActiveFilesFromFirestore()
-        # Remove files that are in the hashmap
-        activeFiles = [file for file in activeFiles if file.file_uuid not in hashmap]
-        if not activeFiles:
-            print("No files found with PENDING status.")
-            return  # Return early if no files to process
-        # Select the first n oldest files
-        filesToScan = selectFirstOldestFiles(data['antivirus']['file-scan-limit'], activeFiles)
-        for file in filesToScan:
-            print(file.group_uuid, file.file_uuid, file.path, file.scan_status)
-
-        # Get the files from cloud storage
-        downloadFilesFromCloudStorage(filesToScan)
-
-        # Scan and update the scanStatus in Firestore
-        for file in filesToScan:
-            res = sendToAPI(file)
-            if res.status == 200:
-                associateFileToAnalysisID(file, res.json()['data'])
-            else:
-                db.collection("files").document(file.file_uuid).update({"scanStatus": FileScanStatus.ERROR.value})
-
-        # Remove the files from the temp directory
-        for file in filesToScan:
-            try:
-                os.remove(tempDirectory + file.file_uuid + "." + getExtension(file.path))
-            except OSError as e:
-                print(f"Error deleting file {file.file_uuid}: {e}")
-
-    except Exception as e:
-        print(f"An error occurred while scanning files: {e}")
-        import traceback
-        traceback.print_exc()
 
 
 def safe_execution(task_function):
@@ -346,32 +274,28 @@ def safe_execution(task_function):
 
 # Schedule the scanNewFiles function to run every 2 minute using the safe_execution wrapper
 schedule.every(60).seconds.do(safe_execution(scanNewFiles))
-schedule.every(60).seconds.do(safe_execution(followUpFileAnalysis))
+schedule.every(20).seconds.do(safe_execution(followUpFileAnalysis))
 
 def run_scheduler():
     while True:
-        schedule.run_pending()  # Check if any scheduled tasks are due to run
-        time.sleep(1)  # Sleep for a short time to prevent high CPU usage
+        schedule.run_pending()
+        time.sleep(1)
 
 
 @app.route('/')
 def home():
-    # Return the contents of the file as a string
     return str("API is running")
 
 
 if __name__ == '__main__':
     logging.info("Running specific tests: test_unit.py and test_system.py")
 
-    # List of specific test files to run
     test_files = ['test_unit.py', 'test_system.py']
 
-    # Run pytest with specific test files and capture the exit code
     result = subprocess.run(
         [sys.executable, '-m', 'pytest', '--junitxml=./test-results.xml'] + test_files
     )
 
-    # Check the result and exit if tests fail
     if result.returncode != 0:
         logging.error("Tests failed. Exiting...")
         sys.exit(result.returncode)
@@ -380,8 +304,6 @@ if __name__ == '__main__':
     scheduler_thread = threading.Thread(target=run_scheduler)
     scheduler_thread.daemon = True  # Daemonize thread to ensure it exits when main program exits
     scheduler_thread.start()
-
-    
 
     # Start the Flask application
     serve(app, host="0.0.0.0", port=8080)
